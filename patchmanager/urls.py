091c1402bb818876e48adc30f17493a5f646ea37
mingjaekim <matsalt@matui-MacBookPro.local>
2025-05-24 21:56:10 +0900

    🚀 루트 URL을 /upload로 리디렉트

--- patchmanager/urls.py
+++ patchmanager/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('patchlog.urls')), 
+    path('', lambda request: redirect('upload_patch')),  # ← 이 줄 추가!
+    path('', include('patchlog.urls')),
 ]
